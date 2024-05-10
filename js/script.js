2f3a5415300ebf6a8191518ba263a169f56100b8
Marcelo Motto <marcelo.motto75@gmail.com>
2024-05-10 16:49:48 -0300

    actualizando formulario

--- js/script.js
+++ js/script.js
@@ -4,26 +4,78 @@ let closeButton = document.getElementsByClassName("close-button")[0];
 let submitButton = document.getElementById("submitButton");
 let form = document.getElementById("contactForm");
 
-// Mostrar la ventana de diálogo al hacer clic en el botón "Enviar"
-// submitButton.addEventListener("click", function() {
-//   dialogContainer.style.display = "block";
-// });
-
   // Mostrar la ventana de diálogo al enviar el formulario si los campos son válidos
   form.addEventListener("submit", function(event) {
     event.preventDefault(); // Evitar que se envíe el formulario
 
-    // Verificar que todos los campos sean válidos
-    if (form.checkValidity()) {
-        alert("Todo normal");
-        dialogContainer.style.display = "block";
+let campoOculto = document.getElementsByClassName("campoObligatorio");
+let names = document.getElementById("nombre").value;
+let tel = document.getElementById("telefono").value;
+let email = document.getElementById("email").value;
+let message = document.getElementById("mensaje").value;
+let valid = true;
+
+    if(!names || !email || !tel || !message) {
+        alert("Por favor complete los campos requeridos"+" el tel es: "+tel);
+        valid = false;
     } else {
-      // Si algún campo no es válido, enviar el formulario normalmente
-      alert("Todo NO normal");
-      dialogContainer.style.display = "block";
-      //form.submit();
+        if(message.length < 50) {
+            alert("Por favor escriba un mensaje de al menos 50 caracteres");
+            valid = false;
+        } else {
+            if (!validateEmail(email)) {
+                alert('Por favor, ingrese un correo electrónico válido.');
+                valid = false;
+            } else {
+                valid = true;
+            }
+        }
     }
-  });
+    if (!valid) {
+
+        const esperarEfecto = () => {
+            return new Promise((resolve) => {
+              setTimeout(() => {
+                resolve();
+              }, 2500);
+            });
+          };
+      
+          // Esperar el efecto y luego ocultar el div
+          esperarEfecto().then(() => {
+            setTimeout(() => {
+                for (let i = 0; i < campoOculto.length; i++) {
+                    campoOculto[i].style.display = "inline";
+                }
+            }, 2500); // Añade un pequeño retraso antes de ocultar completamente el div
+          });
+    }
+
+    function validateEmail(email) {
+        var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
+
+
+
+
+
+
+
+
+
+
+    // Verificar que todos los campos sean válidos
+ //   if (form.checkValidity()) {
+ //       alert("Todo normal");
+ //       dialogContainer.style.display = "block";
+ //   } else {
+      // Si algún campo no es válido, enviar el formulario normalmente
+ //     alert("Todo NO normal");
+ //     dialogContainer.style.display = "block";
+      //form.submit();
+ //   }
+ });
 
 
 // Cerrar la ventana de diálogo al hacer clic en el botón de cierre
