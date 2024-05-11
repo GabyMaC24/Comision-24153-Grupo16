ec31c6fea30c044d76753c8fb17de12535f40b45
Marcelo Motto <marcelo.motto75@gmail.com>
2024-05-11 13:51:36 -0300

    actualizando formulario 11/5/24

--- js/script.js
+++ js/script.js
@@ -4,23 +4,22 @@ let closeButton = document.getElementsByClassName("close-button")[0];
 let submitButton = document.getElementById("submitButton");
 let form = document.getElementById("contactForm");
 
-  // Mostrar la ventana de diálogo al enviar el formulario si los campos son válidos
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Evitar que se envíe el formulario
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-let campoOculto = document.getElementsByClassName("campoObligatorio");
-let names = document.getElementById("nombre").value;
-let tel = document.getElementById("telefono").value;
-let email = document.getElementById("email").value;
-let message = document.getElementById("mensaje").value;
-let valid = true;
+    let campoOculto = document.getElementsByClassName("campoObligatorio");
+    let valid = true;
 
-    if(!names || !email || !tel || !message) {
-        alert("Por favor complete los campos requeridos"+" el tel es: "+tel);
+    let names = document.getElementById("nombre").value;
+    let tel = document.getElementById("telefono").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("mensaje").value;
+
+
+    if (!names || !email || !tel || !message) {
         valid = false;
     } else {
-        if(message.length < 50) {
-            alert("Por favor escriba un mensaje de al menos 50 caracteres");
+        if (message.length < 50) {
             valid = false;
         } else {
             if (!validateEmail(email)) {
@@ -32,60 +31,41 @@ let valid = true;
         }
     }
     if (!valid) {
+             submitButton.classList.add("button-container-moved"); //
 
-        const esperarEfecto = () => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                resolve();
-              }, 2500);
-            });
-          };
-      
-          // Esperar el efecto y luego ocultar el div
-          esperarEfecto().then(() => {
-            setTimeout(() => {
-                for (let i = 0; i < campoOculto.length; i++) {
-                    campoOculto[i].style.display = "inline";
-                }
-            }, 2500); // Añade un pequeño retraso antes de ocultar completamente el div
-          });
+        for (let i = 0; i < campoOculto.length; i++) {
+            //submitButton.classList.add("button-container-moved"); //
+            campoOculto[i].classList.add("visible");
+            campoOculto[i].style.display ="inline";
+        }
+        setTimeout(function() {
+            for (let i = 0; i < campoOculto.length; i++) {
+                campoOculto[i].classList.remove("visible");
+                campoOculto[i].style.display ="none";
+                submitButton.classList.remove("button-container-moved"); //
+            }
+        }, 3000);
+
+    } else {
+        //alert('envio formulario');
+        //form.submit();
+        dialogContainer.style.display = "block";
     }
 
     function validateEmail(email) {
         var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return re.test(email);
     }
-
-
-
-
-
-
-
-
-
-
-    // Verificar que todos los campos sean válidos
- //   if (form.checkValidity()) {
- //       alert("Todo normal");
- //       dialogContainer.style.display = "block";
- //   } else {
-      // Si algún campo no es válido, enviar el formulario normalmente
- //     alert("Todo NO normal");
- //     dialogContainer.style.display = "block";
-      //form.submit();
- //   }
- });
-
+});
 
 // Cerrar la ventana de diálogo al hacer clic en el botón de cierre
-closeButton.onclick = function() {
-  dialogContainer.style.display = "none";
+closeButton.onclick = function () {
+    dialogContainer.style.display = "none";
 }
 
 // Cerrar la ventana de diálogo al hacer clic fuera de ella
-window.onclick = function(event) {
-  if (event.target == dialogContainer) {
-    dialogContainer.style.display = "none";
-  }
-}
+// window.onclick = function (event) {
+//     if (event.target == dialogContainer) {
+//         dialogContainer.style.display = "none";
+//     }
+// }
