43567f880a95d177dd01c2a7fd0c81e29df1b8ea
GabyMaC24 <168577983+GabyMaC24@users.noreply.github.com>
2024-05-27 15:48:02 -0300

    Cambio de animación marquee
    de css a javascript

---
+++ js/scrollText.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var marquee = $('.marquee');
+    var indent = marquee.width();
+    
+    marquee.marquee = function() {
+      indent--;
+      marquee.css('text-indent', indent);
+      if (indent < -1 * marquee.children('.marquee-text').width()) {
+        indent = marquee.width();
+      }
+    };
+    
+    marquee.data('interval', setInterval(marquee.marquee, 60 / 60));
+  });
